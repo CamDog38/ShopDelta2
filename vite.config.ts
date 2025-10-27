7377b3b383f8b51d5a71e7c4bc93e5d3125a0c29
CamDog38 <cameron.becker@outlook.com>
2025-10-27 16:58:46 +0400

    Excel export 5

--- vite.config.ts
+++ vite.config.ts
@@ -66,6 +66,9 @@ export default defineConfig({
   ],
   build: {
     assetsInlineLimit: 0,
+    rollupOptions: {
+      external: ["xlsx"],
+    },
   },
   optimizeDeps: {
     include: ["@shopify/app-bridge-react", "@shopify/polaris"],
